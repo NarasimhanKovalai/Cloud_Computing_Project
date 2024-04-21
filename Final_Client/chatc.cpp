05023f1d8af01c497933823d757ffa501a804197
narkov <narkov@DESKTOP-0OPTP0A.localdomain>
2024-04-21 14:00:12 +0530

    Removed unnecessary functions

--- Final_Client/chatc.cpp
+++ Final_Client/chatc.cpp
@@ -85,7 +85,6 @@ void erase_all_users();
 /* Add user to userList */
 void user_add(msg_peer_t *msg);
 /* Delete user from userList */
-void user_delete(msg_down_t *msg);
 /*get localIP Address using socket_fd*/
 in_addr_t sockfd_to_in_addr_t(int sockfd);
 /*function that connects client to the server sends MSG_UP and gets MSG_ACK*/
@@ -137,19 +136,6 @@ int main(int argc, char *argv[])
 	memset(&server_conn, 0, sizeof(struct sockaddr_in));
 	memset(&incoming_sck, 0, sizeof(struct sockaddr_in));
 	memset(&out_sck, 0, sizeof(struct sockaddr_in));
-	/*First*/
-	/*the client connects to the server sends MSG_UP and gets MSG_ACK*/
-
-	/*I commented from here*/
-	// connect_server(&server_conn, &server_assigned_port, &server_fd,
-	// 			   &localIP, (char *)&usr_input);
-	// printf("Congratulations, your port number as assigned by the server "
-	// 	   "is:%d\n",
-	// 	   server_assigned_port.m_port);
-	// printf("Time received from server: %d",
-	// 	   server_assigned_port.starter_time);
-
-	/**I commented till here*/
 
 	cout << "Enter your name :\n";
 	string name;
@@ -163,21 +149,6 @@ int main(int argc, char *argv[])
 	cin >> server_assigned_port.starter_time;
 	cout << "Time received from server: " << server_assigned_port.starter_time << "\n";
 
-	/*now the "Client" needs to start listen to incoming connections in a
-	 * new thread*/
-	/***************************** The Algorithm
-	 ********************************************************* *The
-	 *algorithm is : we are listing for incoming connections at all time **
-	 **if someone sends us MSG_CONN and we are at prompt, meaning not trying
-	 *to connect to anyone      ** *we accept the connection and going to
-	 *chat mode(opening a new terminal)                         ** *if
-	 *someone is trying to connect with us and we are busy we will send a
-	 *"busy" tone = MSG_RESP = 0* *meantime as long we don't have a incoming
-	 *connection we will present the user with menu and ask ** *him what he
-	 *want to do **
-	 ****************************************************************************************************/
-
-	/*create "another" main for incoming connections in thread*/
 	getListFromServer(&server_conn);
 	if (pthread_create(&listen_tid, NULL, listenMode, (void *)&server_assigned_port) != 0)
 		perror("could not create thread");
@@ -261,26 +232,6 @@ void user_add(msg_peer_t *msg)
 	}
 }
 
-/* Delete user from userList */
-void user_delete(msg_down_t *msg)
-{
-	int i;
-	for (i = 0; i < MAX_USERS; i++)
-	{
-		if (listOfPeers[i])
-		{
-			if (listOfPeers[i]->m_addr == msg->m_addr &&
-				listOfPeers[i]->m_port == msg->m_port)
-			{
-				// free the content of the cell
-				free(listOfPeers[i]);
-				listOfPeers[i] = 0;
-				peers_count--;
-				return;
-			}
-		}
-	}
-}
 /*get localIP Address using socket_fd*/
 in_addr_t sockfd_to_in_addr_t(int sockfd)
 {
@@ -381,41 +332,6 @@ void connect_server(struct sockaddr_in *server_conn,
 	puts("Success : got MSG_ACK message");
 }
 
-/*This function is used to fork and execute a chat program in a new terminal*/
-// int openChat(int fd, char inviter[], char accepter[], int LampClock)
-// {
-// 	char ascii_fd[1] = {fd + '0'};
-// 	pid_t child_pid;
-// 	/* Duplicate this process. */
-// 	child_pid = fork();
-// 	if (child_pid != 0)
-// 		/* This is the parent process. */
-// 		return child_pid;
-// 	else
-// 	{
-// 		/* Now execute CHAT */
-// 		/*Note! because we are using fork we have the fd of
-// connected_client*/
-// 		/*The Program we are running : is doing  = While loop chat until
-// MSG_END*/
-// 		// char *args[] = {"/usr/bin/xterm",
-// "-e","./ChatBasedOnFD",ascii_fd, NULL }; 		char temp1[100] =
-// "Inviter"; 		char temp2[100] = "Accepter"; 		char title[200];
-// strcpy(title, temp1); 		strcat(title, inviter);
-// strcat(title, temp2); 		strcat(title, accepter);
-// char *args[] =
-// {"/usr/bin/xterm", "-xrm", "'XTerm.vt100.allowTitleOps: false'", "-T", title,
-// "-e", "./ChatBasedOnFD", ascii_fd, NULL}; 		execv("/usr/bin/xterm",
-// args);
-// 		/* The execl function returns only if an error occurs. */
-// 		// fprintf(stderr, "an error occurred in execlp\n");
-// 		// abort();
-// 	}
-// 	return 0;
-// }
-
-/*function that opens a the client for incoming connection(runs in a new
- * thread)*/
 void *listenMode(void *args)
 {
 	static sigset_t mask;
@@ -461,15 +377,10 @@ void *listenMode(void *args)
 	// getListFromServer(&addr);
 	for (int i = 0; i < MAX_USERS; i++)
 	{
-		cout << "I am inside for loop lissten mode" << endl;
 		if (listOfPeers[i])
 		{
-			cout << "Checking port matching"
-				 << listOfPeers[i]->m_port << " "
-				 << actualArgs->m_port << endl;
 			if (listOfPeers[i]->m_port == actualArgs->m_port)
 			{
-
 				reply_received_from_index = i;
 				break;
 			}
@@ -523,74 +434,8 @@ void *listenMode(void *args)
 /*When user sends MSG_WHO this method will get all users from our server*/
 void getListFromServer(struct sockaddr_in *server_conn)
 {
-	/*create a new sockaddr for a new connection*/
-	// struct sockaddr_in serv_who_peer;
-	// memset(&serv_who_peer, 0, sizeof(serv_who_peer));
-	// serv_who_peer.sin_family = AF_INET; // IPv4 Structure
-	// /*reusing struct sockaddr_in in main*/
-	// serv_who_peer.sin_addr = server_conn->sin_addr;
-	// serv_who_peer.sin_port = htons(C_SRV_PORT); // port num defined in chat.h
 
-	// int server_fd = 3; // that number will be erased by socket
-	// // open socket
-
-	// if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-	// {
-	// 	cout << "\n Error : Could not create socket \n";
-	// 	exit(1);
-	// }
-
-	// if (connect(server_fd, (struct sockaddr *)&serv_who_peer, sizeof(*server_conn)) == -1)
-	// {
-	// 	perror("connect to server to get MSG_WHO refused");
-	// 	exit(1);
-	// }
-
-	// // send MSG_WHO to our server
-	// msg_who_t sendToServer;
-	// sendToServer.m_type = MSG_WHO;
-
-	// // Send MSG_WHO
-	// if (send(server_fd, (void *)&sendToServer, sizeof(sendToServer), 0) < 0)
-	// {
-	// 	cout << "Sending MSG_WHO failed";
-	// 	exit(1);
-	// }
-	// cout << "MSG_WHO Sent to server\n";
-
-	// sleep(1);
-
-	// // get MSG_HDR message from server and start getting the list of connected users
-	// msg_hdr_t HdrMsgFromServer;
-	// if (recv(server_fd, &HdrMsgFromServer, sizeof(msg_hdr_t), 0) == -1)
-	// {
-	// 	perror("read Messege \"MSG_HDR\" fail");
-	// 	exit(1);
-	// }
-	// cout << "Success : got MSG_HDR message";
-	// sleep(1);
-	// /*in loop of number of connected users fill our array(peer_msg array) with data*/
-
-	// cout << "Got " << HdrMsgFromServer.m_count<< " peers from Server:...\n";
-
-	// clear array from previous results
 	erase_all_users();
-
-	// for (int i = 0; i < HdrMsgFromServer.m_count; i++)
-	// {
-	// 	msg_peer_t *PeerMsgFromServer = (msg_peer_t *)malloc(sizeof(msg_peer_t));
-	// 	if (!PeerMsgFromServer)
-	// 		perror("malloc"); // check if malloc failed
-	// 	if (recv(server_fd, PeerMsgFromServer, sizeof(msg_peer_t), 0) == -1)
-	// 	{
-	// 		// perror("recv read Messege \"MSG_PEER\" fail");
-	// 		fprintf(stderr, "Error receiving message: %s\n", strerror(errno));
-	// 		exit(1);
-	// 	}
-
-	// 	// save with our array  external function
-	// 	user_add(PeerMsgFromServer);
-	// }
 
 	for (int i = 0; i < 3; i++)
 	{
@@ -627,142 +472,6 @@ void getListFromServer(struct sockaddr_in *server_conn)
 		user_add(PeerMsgFromServer);
 	}
 }
-
-/*I have commented from here*/
-// /*When user sends MSG_WHO this method will get all users from our server*/
-// void getListFromServer(struct sockaddr_in *server_conn)
-// {
-// 	/*create a new sockaddr for a new connection*/
-// 	struct sockaddr_in serv_who_peer;
-// 	memset(&serv_who_peer, 0, sizeof(serv_who_peer));
-// 	serv_who_peer.sin_family = AF_INET; // IPv4 Structure
-// 	/*reusing struct sockaddr_in in main*/
-// 	serv_who_peer.sin_addr = server_conn->sin_addr;
-// 	serv_who_peer.sin_port =
-// 		htons(C_SRV_PORT); // port num defined in chat.h
-
-// 	int server_fd = 3; // that number will be erased by socket
-// 	// open socket
-
-// 	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-// 	{
-// 		printf("\n Error : Could not create socket \n");
-// 		exit(1);
-// 	}
-
-// 	if (connect(server_fd, (struct sockaddr *)&serv_who_peer,
-// 				sizeof(*server_conn)) == -1)
-// 	{
-// 		perror("connect to server to get MSG_WHO refused");
-// 		exit(1);
-// 	}
-
-// 	// send MSG_WHO to our server
-// 	msg_who_t sendToServer;
-// 	sendToServer.m_type = MSG_WHO;
-
-// 	// Send MSG_WHO
-// 	if (send(server_fd, (void *)&sendToServer, sizeof(sendToServer), 0) <
-// 		0)
-// 	{
-// 		puts("Sending MSG_WHO failed");
-// 		exit(1);
-// 	}
-// 	puts("MSG_WHO Sent to server\n");
-
-// 	sleep(1);
-
-// 	// get MSG_HDR message from server and start getting the list of
-// 	// connected users
-// 	msg_hdr_t HdrMsgFromServer;
-// 	if (recv(server_fd, &HdrMsgFromServer, sizeof(msg_hdr_t), 0) == -1)
-// 	{
-// 		perror("read Messege \"MSG_HDR\" fail");
-// 		exit(1);
-// 	}
-// 	puts("Success : got MSG_HDR message");
-// 	sleep(1);
-// 	/*in loop of number of connected users fill our array(peer_msg array)
-// 	 * with data*/
-
-// 	printf("Got %d peers from Server:...\n", HdrMsgFromServer.m_count);
-
-// 	// clear array from previous results
-// 	erase_all_users();
-
-// 	for (int i = 0; i < HdrMsgFromServer.m_count; i++)
-// 	{
-// 		msg_peer_t *PeerMsgFromServer =
-// 			(msg_peer_t *)malloc(sizeof(msg_peer_t));
-// 		if (!PeerMsgFromServer)
-// 		{
-// 			perror("malloc"); // check if malloc failed
-// 		}
-// 		if (recv(server_fd, PeerMsgFromServer, sizeof(msg_peer_t), 0) ==
-// 			-1)
-// 		{
-// 			// perror("recv read Messege \"MSG_PEER\" fail");
-// 			fprintf(stderr, "Error receiving message: %s\n",
-// 					strerror(errno));
-// 			exit(1);
-// 		}
-
-// 		// save with our array  external function
-// 		user_add(PeerMsgFromServer);
-// 	}
-// }
-
-// /*Remove peer from server*/
-// void removePeerFromServer(struct sockaddr_in *server_conn,
-// 						  msg_ack_t *server_assigned_port)
-// {
-// 	/*create a new sockaddr for a new connection*/
-// 	struct sockaddr_in serv_who_peer;
-// 	memset(&serv_who_peer, 0, sizeof(serv_who_peer));
-// 	serv_who_peer.sin_family = AF_INET; // IPv4 Structure
-// 	/*reusing struct sockaddr_in in main*/
-// 	serv_who_peer.sin_addr = server_conn->sin_addr; // opened to any IP
-// 	serv_who_peer.sin_port =
-// 		htons(C_SRV_PORT); // port num defined in chat.h
-
-// 	int server_fd = 3; // that number will be erased by socket
-// 	// open socket
-
-// 	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-// 	{
-// 		printf("\n Error : Could not create socket \n");
-// 		exit(1);
-// 	}
-
-// 	if (connect(server_fd, (struct sockaddr *)&serv_who_peer,
-// 				sizeof(*server_conn)) == -1)
-// 	{
-// 		perror("connect to server to send MSG_DOWN refused");
-// 		exit(1);
-// 	}
-
-// 	// send MSG_DOWN to our server
-// 	msg_down_t sendToServer;
-// 	sendToServer.m_type = MSG_DOWN;
-// 	sendToServer.m_port = server_assigned_port->m_port;
-// 	sendToServer.m_addr = sockfd_to_in_addr_t(server_fd);
-
-// 	// Send MSG_DOWN
-// 	if (send(server_fd, (void *)&sendToServer, sizeof(sendToServer), 0) <
-// 		0)
-// 	{
-// 		puts("Sending MSG_DOWN failed");
-// 		exit(1);
-// 	}
-// 	puts("MSG_DOWN Sent to server\n");
-// 	puts("Closing All Important Things..., Goodbye\n");
-// 	puts("It's OK to Close the Window Now OR enter ctrl+c\n");
-// 	pthread_exit(&listen_tid);
-// 	pthread_cancel(listen_tid);
-// 	pthread_kill(listen_tid, SIGKILL);
-// 	exit(1);
-// }
-/*I commented till here*/
 
 void *handlePeerConnection(void *tArgs)
 {
@@ -841,29 +550,6 @@ void *handlePeerConnection(void *tArgs)
 		cout << message_reply << endl;
 		cout << "Give reply index to broadcaster as  "
 			 << reply_received_from_index << endl;
-		// cout<<"Hello";
-		// cout<<"The port of broadcaster is
-		// :"<<listOfPeers[atoi(pidstring)]->m_port; cout<<"World";
-		// struct sockaddr_in *out_sock;
-		// out_sock->sin_family = AF_INET;
-		// out_sock->sin_addr.s_addr =
-		// listOfPeers[atoi(pidstring)]->m_addr; out_sock->sin_port =
-		// listOfPeers[atoi(pidstring)]->m_port;
-		// // open socket
-		// if ((equlsPeerFD = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-		// 	printf("\n Error : Could not create "
-		// 	       "socket \n");
-		// 	exit(1);
-		// }
-
-		// if (connect(equlsPeerFD, (struct sockaddr *)out_sock,
-		// 	    sizeof(*out_sock)) < 0) {
-		// 	perror("Connected to receiving thread of brodcaster");
-		// 	exit(1);
-		// }
-
-		// auto check = send(equlsPeerFD, message_reply.c_str(),
-		// message_reply.length() + 1, 0);
 
 		auto check = send(*client_fd, message_reply.c_str(),
 						  message_reply.length() + 1, 0);
@@ -916,140 +602,6 @@ void *handlePeerConnection(void *tArgs)
 	pthread_detach(pthread_self());
 	return 0;
 }
-
-/*the peer choose which peer he wants to connect with*/
-// void selectPeerToConnect(struct sockaddr_in *out_sock, msg_ack_t
-// *server_assigned_port, in_addr_t *localIP, char usr_input[C_BUFF_SIZE])
-// {
-// 	/*Function VARS*/
-
-// 	int equlsPeerFD = -1; // used to open a new chat windows using FD Number
-// 	msg_peer_t peerSelection;
-// 	int userSelection = -1;
-// 	/****************/
-// 	/*print all connected Peers*/
-// 	for (int i = 0; i < MAX_USERS; i++)
-// 	{
-// 		/*Send MSG_PEER messeges*/
-// 		if (listOfPeers[i])
-// 		{
-// 			/*print connected users*/
-// 			printf("\n[%d]\t-\t Username : %s\n", i,
-// listOfPeers[i]->m_name); 			printf("[%d]\t-\t IP : %s\n", i,
-// inet_ntoa(*(struct in_addr *)&listOfPeers[i]->m_addr));
-// printf("[%d]\t-\t Port : %d\n\n", i, listOfPeers[i]->m_port);
-// 		}
-// 	}
-
-// 	// int logFile = open("log.txt", O_WRONLY | O_CREAT | O_APPEND, 0666);
-// 	// if (logFile == -1)
-// 	// {
-// 	// 	perror("Failed to open log file");
-// 	// 	exit(EXIT_FAILURE);
-// 	// }
-
-// 	// // Save the current file descriptor for stdout
-// 	// int stdoutBackup = dup(STDOUT_FILENO);
-// 	// if (stdoutBackup == -1)
-// 	// {
-// 	// 	perror("Failed to backup stdout");
-// 	// 	exit(EXIT_FAILURE);
-// 	// }
-
-// 	// // Redirect stdout to the log file
-// 	// if (dup2(logFile, STDOUT_FILENO) == -1)
-// 	// {
-// 	// 	perror("Failed to redirect stdout");
-// 	// 	exit(EXIT_FAILURE);
-// 	// }
-
-// 	// // Print statements here will be redirected to the log file
-
-// 	// /*Let us randomly choose to which peer he want's to connect*/
-// 	printf("Choose peer number to send to \n");
-// 	// printf("Choosing a random client to send to : \n");
-// 	scanf("%d", &userSelection);
-
-// 	// while (strcmp(listOfPeers[userSelection]->m_name, usr_input) == 0)
-// 	// {
-// 	// 	userSelection = rand() % 3;
-// 	// }
-// 	// scanf("%d", &userSelection);
-// 	printf("Send Event : %s is sending to %s\n ", usr_input,
-// listOfPeers[userSelection]->m_name); 	printf("Updated Lamport Clock of
-// Sender is %d\n", LampClock);
-
-// 	/*Testing Purps*/
-// 	// printf("%d",userSelection);
-// 	/*create msg_conn_t with all data required*/
-// 	msg_conn_t sendToPeer;
-// 	sendToPeer.m_type = MSG_CONN;
-// 	strcpy(sendToPeer.m_name, usr_input);
-// 	sendToPeer.m_addr = *localIP;
-
-// 	/*Fetch Peer data by user choice*/
-
-// 	/*******************************/
-// 	if (listOfPeers[userSelection] != 0)
-// 	{
-// 		peerSelection.m_addr = listOfPeers[userSelection]->m_addr;
-// 		peerSelection.m_port = listOfPeers[userSelection]->m_port;
-// 		peerSelection.m_type = listOfPeers[userSelection]->m_type;
-// 		strcpy(peerSelection.m_name,
-// listOfPeers[userSelection]->m_name);
-
-// 		printf("\nYou choose: %s\n", peerSelection.m_name);
-// 		printf("The IP:port of Requested peers is : %s:%d\n",
-// inet_ntoa(*(struct in_addr *)&peerSelection.m_addr), peerSelection.m_port);
-
-// 		/*open socket, connect to other peer, get FD, send msg_conn_t
-// and wait for RESPONSE from other PEER */ 		out_sock->sin_family =
-// AF_INET; 		out_sock->sin_addr.s_addr = peerSelection.m_addr;
-// out_sock->sin_port = peerSelection.m_port;
-// 		// open socket
-// 		if ((equlsPeerFD = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-// 		{
-// 			printf("\n Error : Could not create socket \n");
-// 			exit(1);
-// 		}
-
-// 		if (connect(equlsPeerFD, (struct sockaddr *)out_sock,
-// sizeof(*out_sock)) < 0)
-// 		{
-// 			perror("connect");
-// 			exit(1);
-// 		}
-
-// 		printf("Sending to  -%s-  Sending MSG_CONN and Waiting for
-// Response...\n\n", peerSelection.m_name);
-
-// 		char lampclockstring[20]; // Allocate a buffer to hold the
-// converted string 		char pidstring[20];		  // Allocate a
-// buffer to hold the converted string
-// 		// Convert the integer to a string using sprintf
-// 		sprintf(lampclockstring, "%d", LampClock);
-// 		sprintf(pidstring, "%d", getpid());
-// 		string message = lampclockstring;
-// 		message += ",";
-// 		message += pidstring;
-
-// 		// Push the message with timestamp and PID into the queue
-// 		messageQueue.push(Message(message, LampClock, getpid()));
-// 		send(equlsPeerFD, message.c_str(), message.length() + 1, 0);
-// 		//
-// openChat(equlsPeerFD,usr_input,peerSelection.m_name,LampClock);
-// 	}
-
-// 	// // Restore stdout to its original file descriptor
-// 	// if (dup2(stdoutBackup, STDOUT_FILENO) == -1)
-// 	// {
-// 	// 	perror("Failed to restore stdout");
-// 	// 	exit(EXIT_FAILURE);
-// 	// }
-
-// 	// // Close the log file
-// 	// close(logFile);
-// }
 
 void Broadcast(struct sockaddr_in *out_sock, msg_ack_t *server_assigned_port,
 			   in_addr_t *localIP, char usr_input[C_BUFF_SIZE], int broadcast_type)
@@ -1245,8 +797,9 @@ void Broadcast(struct sockaddr_in *out_sock, msg_ack_t *server_assigned_port,
 							{
 								requested_for_cs = 0;
 								cout << "Peer " << broadcaster_index << "is allowed to access the critical section !" << endl;
+								cout << "ENTERING THE CRITICAL SECTION!!!" << endl;
 								sleep(5);
-
+								cout<<"LEAVING THE CRITICAL SECTION!!!"<<endl;
 								cout << "Peer " << broadcaster_index << " is about to broadcast a release" << endl;
 								/*Release Critical Section now by popping the broadcaster's entry from each queue*/
 								char lampclockstring[20]; // Allocate a buffer to hold the converted
@@ -1268,21 +821,21 @@ void Broadcast(struct sockaddr_in *out_sock, msg_ack_t *server_assigned_port,
 								cout << "Peer " << broadcaster_index << "is denied access to the critical section due to queue rule !" << endl;
 							}
 						}
-						}
-					}
-					std::cout << "\nMessages in sender's queue:\n";
-					priority_queue<Message> tempQueue =
-						messageQueue;
-					while (!tempQueue.empty())
-					{
-						Message msg = tempQueue.top();
-						std::cout << "Content: " << msg.content
-								  << " Timestamp: " << msg.timestamp
-								  << " PID: " << msg.pid << std::endl;
-						tempQueue.pop();
 					}
 				}
+				std::cout << "\nMessages in sender's queue:\n";
+				priority_queue<Message> tempQueue =
+					messageQueue;
+				while (!tempQueue.empty())
+				{
+					Message msg = tempQueue.top();
+					std::cout << "Content: " << msg.content
+							  << " Timestamp: " << msg.timestamp
+							  << " PID: " << msg.pid << std::endl;
+					tempQueue.pop();
+				}
 			}
-			userSelection++;
 		}
+		userSelection++;
 	}
+}
